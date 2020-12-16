6301ea61f6412d12c6bbfd7b5e91c1c2922b1c5d
Alex Holloway <alex.holloway.ctx@gmail.com>
2020-12-16 14:19:00 +0000

    Expression parameters

--- parse-badger-csharp/Models/ExpressionModel.cs
+++ parse-badger-csharp/Models/ExpressionModel.cs
@@ -1,10 +1,22 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace parse_badger_csharp.Models
 {
+    public class ExpressionParameterModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Value { get; set; }
+    }
+
     public class ExpressionModel
     {
         [Required]
         public string Expression { get; set; }
+
+        public List<ExpressionParameterModel> Parameters { get; set; } = new List<ExpressionParameterModel>();
     }
 }
